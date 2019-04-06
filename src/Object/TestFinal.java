5ca46a2d25b3beaa90a1d8bcdddeb05b8e73234d
suhanyujie <suhanyujie@qq.com>
2019-04-06 12:24:25 +0800

    final关键字

---
+++ src/Object/TestFinal.java
@@ -0,0 +1,23 @@
+package Object;
+
+public class TestFinal {
+    final float pi = 3.14F;
+
+    final public static void test1(){
+
+    }
+}
+
+final class LastClass1{
+
+}
+
+
+/*
+## final关键字
+* final修饰变量时，表示是常量，不能对其进行第2次赋值
+* final修饰方法时，表示方法不能被重写，但是可以被重载。重写和重载是不一样的概念！
+* final修饰类时，表示这个类不能被继承。
+
+
+ */
