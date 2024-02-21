d72365f28c46d480839040fd2230d4d72f67a0b1
suhanyujie <suhanyujie@qq.com>
2024-02-21 22:17:30 +0900

    add some code of java note

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -1,5 +1,7 @@
 package hello;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
 //         hello1();
@@ -7,7 +9,14 @@ public class HelloWorld {
 //        System.out.printf(String.format("%d\n", res));
 //        res = 3;
 //        System.out.printf(String.format("%d\n", res));
-        typeImplicitTrans();
+//        typeTrans1();
+//        concatStr1();
+//        multiLineStr1();
+//        defArray1();
+//        strArr1();
+//        readInput1();
+//        switchCases();
+        switchAsExpressive();
     }
 
     /**
@@ -41,6 +50,96 @@ public class HelloWorld {
         short b = 1;
         var s = a + b;
         System.out.printf(String.format("隐式转换：%s\n", s));
+    }
+
+    // 类型转换
+    public static void typeTrans1() {
+        int a0 = 1;
+        short b = 1;
+        a0 = (int) b;
+        System.out.printf("类型转换：%s\n", a0);
+
+        int a = 2147483641;
+        short a2 = (short) a;
+        System.out.printf("超出范围的类型转换 get：%d, expect:%d\n", a2, 2147483641);
+    }
+
+    // 字符串若与数值变量拼接，会先将数值转换为字符串，再进行拼接
+    public static void concatStr1() {
+        int a = 1;
+        String s1 = "hell world ";
+        String s2 = s1 + a;
+        System.out.printf("字符串连接 %s\n", s2);
+    }
+
+    // 多行字符串
+    public static void multiLineStr1() {
+        String s1 = """
+                hello
+                world
+                .
+                """;
+        System.out.printf("多行字符串 %s\n", s1);
+    }
+
+    // 定义数组
+    public static void defArray1() {
+        int[] arr1 = new int[5];
+        int[] arr2 = new int[]{1, 2};
+        int[] arr3 = {1, 2};
+        arr1[0] = arr1[1] = 1;
+        System.out.printf("数组：%s, len:%d\n", arr1, arr1.length);
+    }
+
+    public static void strArr1() {
+        String[] names = {"ABC", "XYZ", "zoo"};
+        String s = names[1];
+        names[1] = "cat";
+        System.out.println(s);
+    }
+
+    public static void readInput1() {
+        Scanner s1 = new Scanner(System.in);
+        String res = "";
+        while (s1.hasNext()) {
+            String tmpStr = s1.nextLine();
+            System.out.printf("input: %s\n", tmpStr);
+            if (tmpStr.equals("exit")) {
+                break;
+            }
+        }
+        System.out.print("input end...\n");
+    }
+
+    // switch 使用时，记得加上 `break`！
+    public static void switchCases() {
+        int op = 2;
+        switch (op) {
+            case 1:
+                System.out.print("1...\n");
+            case 2:
+                System.out.print("2...\n");
+            case 3:
+                System.out.print("3...\n");
+                break;
+            case 4:
+                System.out.print("4...\n");
+        }
+        System.out.print("end...\n");
+    }
+
+    // switch 作为表达式使用
+    public static void switchAsExpressive() {
+        int op = 3;
+        String res = switch (op) {
+            case 1 -> "1";
+            case 2 -> "2";
+            case 3 -> {
+                yield "3"; // 使用 yield 返回一个值作为 switch 表达式的值
+            }
+            default -> "0";
+        };
+        System.out.printf("switch res: %s\n", res);
     }
 }
 
@@ -84,7 +183,31 @@ byte 和 short 类型位移运算时，会先转换为 int 再进行位移运算
 ## 类型提升和强制转换
 和大多数编程语言一样，java 中也存在变量的隐式转换和作用域提升。这都是为了在开发时，有更多的便利。
 
-需要注意的是，超出范围的强制转型会得到**错误**的结果。
+类型转换时，需要注意的是，超出范围的强制转型会得到**错误**的结果。例如：
+
+```java
+int a = 2147483641;
+        short a2 = (short) a;
+        // get: -7
+        System.out.printf("超出范围的类型转换 get：%d, expect:%d\n", a2, 2147483641);
+```
+
+## 字符串
+
+### 字符串拼接
+字符串若与数值变量拼接，会先将数值转换为字符串，再进行拼接
+
+### 特殊的定界符
+java 中的字符串可以使用 `"""` 定义字符串，优点是定义多行字符串时，可读性更好。
+
+```java
+String s1 = """
+hello
+world
+.
+""";
+```
+
 
 
 
